ecbe65e40fd160302dac21c04f8d1d2556c16281
Nadeen Poorna <nadeenpoorna405@gmail.com>
2025-06-10 15:24:39 +0530

    Update gen-id.js

--- gen-id.js
+++ gen-id.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let characters = "AB8CDE3FGHIJKLMNOPQR0STUV9WX4YZa2bcde1fghijklm7nopq6rstu5vwxyz";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
